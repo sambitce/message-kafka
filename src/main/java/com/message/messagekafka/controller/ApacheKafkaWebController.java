a89e9f4ee1700dd74f6177c7e4eb51c72140467e
sambath <sambathkumar.rsk@gmail.com>
2018-03-28 21:27:42 -0500

    Kafka streams example

--- src/main/java/com/message/messagekafka/controller/ApacheKafkaWebController.java
+++ src/main/java/com/message/messagekafka/controller/ApacheKafkaWebController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.message.messagekafka.domain.ItemEventLog;
 import com.message.messagekafka.repository.ItemEventLogRepository;
 import com.message.messagekafka.service.KafkaSender;
+import com.message.messagekafka.service.KafkaStreamsService;
 
 @RestController
 @RequestMapping(value = "/sample-topic")
@@ -25,13 +27,17 @@ public class ApacheKafkaWebController {
 	ItemEventLogRepository itemRepository ; 
 	
 	
+	
+	
+	
 	@PostMapping(value="/publish" )
 	public String producer(@RequestBody ItemEventLog message) {
 		
 		/*** send is used for simple topic publishing in kafka **/ 
 		message.setId(UUID.randomUUID());
-		ItemEventLog createdEvent = itemRepository.save(message);
-		kafkaSender.send(createdEvent);
+		//ItemEventLog createdEvent = itemRepository.save(message);
+		itemRepository.insert(message);
+		kafkaSender.send(message);
 		
 		
 		//mySource.output().send(MessageBuilder.withPayload(message).build());
@@ -51,4 +57,12 @@ public class ApacheKafkaWebController {
 		return "Success" ;
 	}
 	
+	@PostMapping(value="/stream")
+	public String publishStream(@RequestParam("message") String message) {
+		KafkaStreamsService streamsSerivce = new KafkaStreamsService();
+		System.out.println("Stream message is " + message);
+		streamsSerivce.streamData(message);
+	
+		return "Message published in stream input" ;
+	}
 }
